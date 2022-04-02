8a26ab07d673e3ab48c243ef52c8a6dd3da4adea
Christopher <ctijerina@live.com>
2022-04-02 15:19:02 -0500

    added the necessary query and mutation types as outlined in the requirements to the typeDef, still missing the resolvers. Error shows up on nodemon, but will check again after the resolvers are done

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,9 +3,36 @@ const { gql } = require("apollo-server-express");
 
 // create our typeDefs
 const typeDefs = gql`
-	type Query {
-		helloWorld: String
+	type Book {
+		bookID: ID
+		authors: [String]
+		description: String
+		image: String
+		link: String
+		title: String
 	}
+	type User {
+		_id: ID
+		username: String
+		email: String
+		password: String
+		savedBooks: [Book]
+	}
+    type Query {
+        users: [User]
+        me: User
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(bookID: ID, authors: [String], description: String, image: String, link: String, title: String); User
+        removeBook(bookID: String!): User
+    }
+
 `;
 
 // export the typeDefs
