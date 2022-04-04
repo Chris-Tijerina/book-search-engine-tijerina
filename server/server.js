8b0feb9e8b8866d1977fc6185c8ac12211d3d5af
Christopher <ctijerina@live.com>
2022-04-03 20:02:18 -0500

    changed db.once in the server js file and deleted the previous one and it's duplicate

--- server/server.js
+++ server/server.js
@@ -34,12 +34,6 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once("open", () => {
-	app.listen(PORT, () => {
-		console.log(`API server running on port ${PORT}!`);
-	});
-});
-
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../client/build")));
@@ -50,7 +44,5 @@ app.get("*", (req, res) => {
 });
 
 db.once("open", () => {
-	app.listen(PORT, () => {
-		console.log("API server running on port ${PORT}!");
-	});
+	app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
