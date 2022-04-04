b88f355a4facf4ccb32d4ff96f6c184093a8fd5b
Christopher <ctijerina@live.com>
2022-04-03 19:36:10 -0500

    issues with heroku deployment, added deleted material back from server.js in case I accidentally deleted something

--- server/server.js
+++ server/server.js
@@ -43,3 +43,13 @@ db.once("open", () => {
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(_dirname, "../client/build/index.html"));
+});
+
+db.once("open", () => {
+	app.listen(PORT, () => {
+		console.log("API server running on port ${PORT}!");
+	});
+});
